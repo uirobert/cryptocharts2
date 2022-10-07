1972b100f77370ac2347aaf32c3ec913e241ed7c
“Robert <digitalmediaflow@gmail.com>
2022-10-07 00:12:39 -0400

    temp disable ts on dashboard

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -12,7 +12,19 @@
   }
   ```
 */
-import { Fragment, useState } from "react";
+// @ts-nocheck
+import {
+	Fragment,
+	JSXElementConstructor,
+	Key,
+	ReactElement,
+	ReactFragment,
+	JSXElementConstructor,
+	Key,
+	ReactElement,
+	ReactFragment,
+	useState,
+} from "react";
 import { Dialog, Menu, Transition } from "@headlessui/react";
 import {
 	Bars3BottomLeftIcon,
@@ -115,23 +127,39 @@ export default function Example() {
 									</div>
 									<div className='mt-5 h-0 flex-1 overflow-y-auto'>
 										<nav className='space-y-1 px-2'>
-											{navigation.map((item) => (
-												<a
-													key={item.name}
-													href={item.href}
-													className={classNames(
-														item.current
-															? "bg-indigo-800 text-white"
-															: "text-indigo-100 hover:bg-indigo-600",
-														"group flex items-center px-2 py-2 text-base font-medium rounded-md"
-													)}>
-													<item.icon
-														className='mr-4 h-6 w-6 flex-shrink-0 text-indigo-300'
-														aria-hidden='true'
-													/>
-													{item.name}
-												</a>
-											))}
+											{navigation.map(
+												(item: {
+													name:
+														| boolean
+														| Key
+														| ReactElement<
+																any,
+																string | JSXElementConstructor<any>
+														  >
+														| ReactFragment
+														| null
+														| undefined;
+													href: string | undefined;
+													current: any;
+													icon: JSX.IntrinsicAttributes;
+												}) => (
+													<a
+														key={item.name}
+														href={item.href}
+														className={classNames(
+															item.current
+																? "bg-indigo-800 text-white"
+																: "text-indigo-100 hover:bg-indigo-600",
+															"group flex items-center px-2 py-2 text-base font-medium rounded-md"
+														)}>
+														<item.icon
+															className='mr-4 h-6 w-6 flex-shrink-0 text-indigo-300'
+															aria-hidden='true'
+														/>
+														{item.name}
+													</a>
+												)
+											)}
 										</nav>
 									</div>
 								</Dialog.Panel>
@@ -156,23 +184,36 @@ export default function Example() {
 						</div>
 						<div className='mt-5 flex flex-1 flex-col'>
 							<nav className='flex-1 space-y-1 px-2 pb-4'>
-								{navigation.map((item) => (
-									<a
-										key={item.name}
-										href={item.href}
-										className={classNames(
-											item.current
-												? "bg-indigo-800 text-white"
-												: "text-indigo-100 hover:bg-indigo-600",
-											"group flex items-center px-2 py-2 text-sm font-medium rounded-md"
-										)}>
-										<item.icon
-											className='mr-3 h-6 w-6 flex-shrink-0 text-indigo-300'
-											aria-hidden='true'
-										/>
-										{item.name}
-									</a>
-								))}
+								{navigation.map(
+									(item: {
+										name:
+											| boolean
+											| Key
+											| ReactElement<any, string | JSXElementConstructor<any>>
+											| ReactFragment
+											| null
+											| undefined;
+										href: string | undefined;
+										current: any;
+										icon: JSX.IntrinsicAttributes;
+									}) => (
+										<a
+											key={item.name}
+											href={item.href}
+											className={classNames(
+												item.current
+													? "bg-indigo-800 text-white"
+													: "text-indigo-100 hover:bg-indigo-600",
+												"group flex items-center px-2 py-2 text-sm font-medium rounded-md"
+											)}>
+											<item.icon
+												className='mr-3 h-6 w-6 flex-shrink-0 text-indigo-300'
+												aria-hidden='true'
+											/>
+											{item.name}
+										</a>
+									)
+								)}
 							</nav>
 						</div>
 					</div>
